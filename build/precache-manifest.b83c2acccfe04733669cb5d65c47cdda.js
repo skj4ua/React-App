b468632004932ae57a34af00471d5da9a5aa80a6
sonu jha <sonukrj08@gmail.com>
2020-01-04 14:05:01 +0530

    Add files via upload

---
+++ build/precache-manifest.b83c2acccfe04733669cb5d65c47cdda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbd8cb24267495db3c8d8ec487a0a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "460da43e78e8466fd5b4",
+    "url": "/static/css/2.23be29d4.chunk.css"
+  },
+  {
+    "revision": "60b473b2ae3bff5e738a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "460da43e78e8466fd5b4",
+    "url": "/static/js/2.ab6889f7.chunk.js"
+  },
+  {
+    "revision": "d57e69aeadc1d65039037330bbaabf32",
+    "url": "/static/js/2.ab6889f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60b473b2ae3bff5e738a",
+    "url": "/static/js/main.7f54b361.chunk.js"
+  },
+  {
+    "revision": "ddd7cb2c85119cab8a60",
+    "url": "/static/js/runtime-main.35d27632.js"
+  }
+]);
